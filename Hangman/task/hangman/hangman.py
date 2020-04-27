0a4aa4d63b36dbefcb1cecff17b228387389f1bf
TonyNewbie <prostomaster90@gmail.com>
2020-04-27 15:08:07 +0700

    Stage 4 passed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,7 +3,7 @@ from random import choice
 words_list = ['python', 'java', 'kotlin', 'javascript']
 hidden_word = choice(words_list)
 print('H A N G M A N')
-word = input('Guess the word: ')
+word = input(f'Guess the word {hidden_word[:3] + "-" * (len(hidden_word) - 3)}: ')
 if word == hidden_word:
     print('You survived!')
 else:
