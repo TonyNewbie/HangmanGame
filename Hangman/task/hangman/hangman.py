8072f3dd0e991255b614e3e475d117e8513f2023
TonyNewbie <prostomaster90@gmail.com>
2020-04-28 14:20:26 +0700

    Stage 7 passed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,4 +1,7 @@
+# Write your code here
 from random import choice
+from string import ascii_lowercase
+
 words_list = ['python', 'java', 'kotlin', 'javascript']
 right_word = choice(words_list)
 hidden_word = '-' * len(right_word)
@@ -9,17 +12,20 @@ while attempts and hidden_word != right_word:
     print()
     print(hidden_word)
     letter = input('Input a letter: ')
-    if letter in right_word:
-        if letter not in guessed_letters:
-            guessed_letters.add(letter)
-            for j in range(len(right_word)):
-                if letter == right_word[j]:
-                    hidden_word = hidden_word[:j] + letter + hidden_word[j + 1:]
-        else:
-            print('No improvements')
-            attempts -= 1
+    if letter in guessed_letters:
+        print('You already typed this letter')
+    elif len(letter) != 1:
+        print('You should print a single letter ')
+    elif letter not in ascii_lowercase:
+        print('It is not an ASCII lowercase letter ')
+    elif letter in right_word:
+        guessed_letters.add(letter)
+        for j in range(len(right_word)):
+            if letter == right_word[j]:
+                hidden_word = hidden_word[:j] + letter + hidden_word[j + 1:]
     else:
         print('No such letter in the word')
+        guessed_letters.add(letter)
         attempts -= 1
 else:
     if attempts == 0:
